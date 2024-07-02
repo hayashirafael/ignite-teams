732f5befd1965b4f346fb1d6a573847461bf0410
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-07-02 15:28:48 -0300

    ayka2

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { Container, Title } from './styles';
+
+export function Groups() {
+  return (
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+  );
+}
