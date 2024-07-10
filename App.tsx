c601b1b8ed6fc3c67b6680deb0f9542627b6f543
Rafael Hayashi <rafaelhayashi8@gmail.com>
2024-07-10 17:15:01 -0300

    finalizando css-in-js

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Groups } from "@screens/Groups";
+import { Players } from "@screens/Players";
 import theme from "./src/theme";
 import { ThemeProvider } from "styled-components/native";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
@@ -14,7 +14,7 @@ export default function App() {
         backgroundColor={"transparent"}
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,34 @@
 import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
-      <Highlight title='Turmas' subtitle='jogue com sua turma' />
+      <Highlight
+        title='Turmas'
+        subtitle='jogue com sua turma'
+      />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Cadastre uma turma' />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+      />
+      <Button
+        title='Criar nova turma'
+      />
+
     </Container>
   );
 }
